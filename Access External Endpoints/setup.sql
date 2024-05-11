50fc2afce64771ad07f46634124e8fefe501b3f5
Doris Lee <doris.lee@snowflake.com>
2024-05-10 18:27:54 -0700

    new EAI tutorial

---
+++ Access External Endpoints/setup.sql
@@ -0,0 +1,19 @@
+-- Create the HuggingFace external access integration and the network rule it relies on.
+CREATE OR REPLACE NETWORK RULE hf_network_rule
+  MODE = EGRESS
+  TYPE = HOST_PORT
+  VALUE_LIST = ('huggingface.co');
+
+CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION hf_access_integration
+  ALLOWED_NETWORK_RULES = (hf_network_rule)
+  ENABLED = true;
+
+-- Create the Github external access integration and the network rule it relies on.
+CREATE OR REPLACE NETWORK RULE gh_network_rule
+  MODE = EGRESS
+  TYPE = HOST_PORT
+  VALUE_LIST = ('raw.githubusercontent.com','github.com');
+
+CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION gh_access_integration
+  ALLOWED_NETWORK_RULES = (gh_network_rule)
+  ENABLED = true;
