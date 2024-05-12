13073bc3509245a0e5943333a4318edb28c02f95
Doris Lee <doris.lee@snowflake.com>
2024-05-12 12:01:27 -0700

    updated setup and testing CI/CD from dev branch

--- Access External Endpoints/setup.sql
+++ Access External Endpoints/setup.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE NETWORK RULE hf_network_rule
   MODE = EGRESS
   TYPE = HOST_PORT
-  VALUE_LIST = ('huggingface.co');
+  VALUE_LIST = ('huggingface.co','cdn-lfs-us-1.huggingface.co');
 
 CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION hf_access_integration
   ALLOWED_NETWORK_RULES = (hf_network_rule)
@@ -17,3 +17,5 @@ CREATE OR REPLACE NETWORK RULE gh_network_rule
 CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION gh_access_integration
   ALLOWED_NETWORK_RULES = (gh_network_rule)
   ENABLED = true;
+
+ALTER NOTEBOOK GH_ACTION_EAI_SETUP set EXTERNAL_ACCESS_INTEGRATIONS = (hf_access_integration, gh_access_integration);
